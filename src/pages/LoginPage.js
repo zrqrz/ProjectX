524c374a9fc7af1485e83fa7be1f2615778a5ea7
Runqiao Zhang <runqiaoz@uvic.ca>
2023-07-21 14:21:40 -0700

    login page 1st draft finished

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,9 +1,38 @@
 import React from "react";
 
+import loginImg from "../assets/login.png";
+
 function LoginPage() {
   return (
-    <div>
-      <h1>This is Login page!</h1>
+    <div className="relative w-full h-screen">
+      <img
+        className="absolute w-full h-full object-cover mix-blend-overlay"
+        src={loginImg}
+        alt="/"
+      />
+      <div className="flex justify-center items-center h-full">
+        <form className="max-w-[400px] w-full mx-auto bg-white p-2 px-4">
+          <h2 className="text-4xl font-bold text-center py-4">7Code</h2>
+          <div className="flex flex-col py-2">
+            <label>Username</label>
+            <input className="border p-2" type="text" />
+          </div>
+          <div className="flex flex-col py-2">
+            <label>Password</label>
+            <input className="border p-2" type="password" />
+          </div>
+          <button className="border w-full my-5 py-2 bg-indigo-600 hover:bg-indigo-500 text-white">
+            Sign In
+          </button>
+          <div className="flex justify-between">
+            <p>
+              <input type="checkbox" />
+              Remember Me
+            </p>
+            <p>Forgot password?</p>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
